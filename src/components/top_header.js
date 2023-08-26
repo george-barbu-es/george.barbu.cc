9d778c8156c9b25d3dc86018e9703db1a6917ac7
barbugeorgedev <GEorgianna89>
2023-08-27 01:09:22 +0200

    fix profile image responsive

--- src/components/top_header.js
+++ src/components/top_header.js
@@ -6,7 +6,12 @@ export default function TopHeader({data}) {
   return (
     <div className="bg-primary w-full h-44 flex flex-col content-center top-12 justify-end relative pb-4 mt-20 print:pb-0 print:absolute print:justify-center print:h-32 print:mt-0    sm:pb-0 sm:absolute sm:justify-center sm:h-32 sm:mt-0">
         <div className="flex relative w-full">
-            <GatsbyImage className="!absolute w-[180px] -translate-y-24 left-8 mx-auto z-10 print:absolute print:-translate-y-12 sm:absolute sm:-translate-y-12" alt="Profile Image" image={image} />
+            <GatsbyImage className="!relative w-[180px] -translate-y-24 left-0 mx-auto z-10 
+            print:absolute print:-translate-y-12 
+            sm:!absolute  
+            sm:-translate-y-12
+            sm:left-8" 
+            alt="Profile Image" image={image} />
         </div>
         <div className="flex flex-col justify-end content-center text-center absolute w-full print:w-4/6 print:left-1/3 print:text-left print:relative print:justify-center    sm:w-4/6 sm:left-1/3 sm:text-left sm:relative sm:justify-center">
         {data.text &&
